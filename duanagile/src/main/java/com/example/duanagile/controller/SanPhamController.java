a388c9b440d232993032b78bb9645dc46b834343
daongocnam12 <namokk247@gmail.com>
2025-04-02 02:37:35 +0700

    Add files via upload

--- duanagile/src/main/java/com/example/duanagile/controller/SanPhamController.java
+++ duanagile/src/main/java/com/example/duanagile/controller/SanPhamController.java
@@ -1,12 +1,21 @@
 package com.example.duanagile.controller;
 
+import com.example.duanagile.repository.SanPhamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/san-pham")
 public class SanPhamController {
-    @GetMapping("/sanPham/hien-thi")
-    public String hienThiSanPham() {
+    @Autowired
+    private SanPhamRepository sanPhamRepository;
+
+    @GetMapping("/hien-thi")
+    public String hienThi(Model model) {
+        model.addAttribute("danhSachSanPham", sanPhamRepository.findAll());
         return "sanPham/hien-thi";
     }
 }
